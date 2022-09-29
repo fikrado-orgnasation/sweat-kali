05103b066ae0fd84122832a2b3745110a7849687
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2022-09-29 20:06:51 +0300

    Update sweet-kali.sh

--- sweet-kali.sh
+++ sweet-kali.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 unzip Sweet-Dark.zip
 unzip candy-icons.zip
-mv Sweet-Dark /usr/share/themes
-mv candy-icons /usr/share/icons
+sudo mv Sweet-Dark /usr/share/themes
+sudo mv candy-icons /usr/share/icons
+sudo apt install plank stacer figlet
 xfce4-desktop -p insert_property_here -s img/bgg.jpg
+figlet DONE 
